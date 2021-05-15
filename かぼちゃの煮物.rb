593dea3ceeb135a4bf532f75cd5f06f44bc45c54
ongaeshi <ongaeshi0621@gmail.com>
2021-05-15 23:36:46 +0900

    feat: シーン化

--- かぼちゃの煮物.rb
+++ かぼちゃの煮物.rb
@@ -5,35 +5,51 @@ App.end_time = 15
 font = Font.new(50)
 font_s = Font.new(30)
 
+pumpkin = Texture.new(Emoji.new("🎃"), TextureDesc::Mipped)
+
 script do |c|
   Drawer.background("black")
 end
 
 script do |c|
-  t = c.text(font, 60, 120, color: "orange")
-
   # シーン1
-  t.text = "🎃かぼちゃの煮物🎃"
+  c.scene_script do |scene|
+    t = scene.text(font, 120, 120, color: "orange")
+    t.text = "かぼちゃの煮物"
+
+    pumpkin_left = scene.texture(pumpkin, 40, 120)
+    pumpkin_left.scale(0.5, 0.5)
+  
+    pumpkin_right = scene.texture(pumpkin, 480, 120)
+    pumpkin_right.scale(0.5, 0.5)
+
+    scene.until_time 3
+  end
 
   # シーン2
-  c.until_time 3
-  t.x = t.y = 10
-  t.text = "かぼちゃ 1/4\n"
-  c.wait 0.8
-  t.text += "砂糖 大さじ1\n"
-  c.wait 0.8
-  t.text += "醤油 大さじ1\n"
-  c.wait 0.8
+  c.scene_script do |scene|
+    t = scene.text(font, 10, 10, color: "orange")
+
+    t.text = "かぼちゃ 1/4個\n"
+    scene.wait 0.8
+    t.text += "砂糖 大さじ1\n"
+    scene.wait 0.8
+    t.text += "醤油 大さじ1\n"
+    scene.wait 0.8
+
+    scene.until_time 4
+  end
 
   # シーン3
-  c.until_time 7
-  t.font = font_s
-  t.text = "1. かぼちゃ入れて水の高さは1.5cm位\n"
-  c.wait 2
-  t.text += "2. 砂糖入れて蓋して中火で蒸し煮\n"
-  c.wait 2
-  t.text += "3. 醤油入れて鍋を回して煮汁を絡ませる\n"
-  c.wait 2
+  c.scene_script do |scene|
+    t = scene.text(font_s, 10, 10, color: "orange")
+    t.text = "1. かぼちゃを入れて水の高さは1.5cm位\n"
+    scene.wait 2
+    t.text += "2. 砂糖入れて蓋して中火で蒸し煮\n"
+    scene.wait 2
+    t.text += "3. 醤油入れて鍋を回して煮汁を絡ませる\n"
+    scene.wait 2
+  end
 end
 
 App.run
