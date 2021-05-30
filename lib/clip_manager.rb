0d1a637c01a93aaf617340b6af9dfa35e8e208d5
ongaeshi <ongaeshi0621@gmail.com>
2021-05-30 23:49:22 +0900

    feat: ClipScriptを更新(ea750b82)

--- lib/clip_manager.rb
+++ lib/clip_manager.rb
@@ -3,14 +3,15 @@ require 'clip_object'
 module Clip
   class ClipManager
     attr_reader :root, :time
-    attr_accessor :start_time, :end_time, :is_stop
+    attr_accessor :start_time, :end_time, :is_stop, :is_loop
 
-    def initialize(start_time = nil, is_stop = nil)
+    def initialize(start_time = nil, is_stop = nil, is_loop = nil)
       @root = RootClip.new
       @time = 0.0
       @start_time = start_time || 0.0
       @end_time = 4.0
       @is_stop = is_stop || false
+      @is_loop = is_loop.nil? ? true : is_loop
     end
 
     def script(&block)
@@ -42,7 +43,7 @@ module Clip
 
         root.draw
 
-        if @time > @end_time
+        if @time > @end_time && @is_loop
           @time = 0
 
           root.children.each do |c|
@@ -52,7 +53,7 @@ module Clip
 
         prev_time = @time
 
-        @time, @is_stop = timeline_ui(@time, @end_time, @is_stop)
+        @time, @is_stop, @is_loop = timeline_ui(@time, @end_time, @is_stop, @is_loop)
       
         unless @is_stop
           delta_time = min_delta_time
@@ -70,20 +71,12 @@ module Clip
           end
         end
       end
-    end  
+    end
   end
 
-  if Object.const_defined?("CLIP_MANAGER_START_TIME")
-    if Object.const_defined?("CLIP_MANAGER_IS_STOP")
-      $clip_manager = ClipManager.new(CLIP_MANAGER_START_TIME, CLIP_MANAGER_IS_STOP)
-    else
-      $clip_manager = ClipManager.new(CLIP_MANAGER_START_TIME)
-    end
-  else
-    if Object.const_defined?("CLIP_MANAGER_IS_STOP")
-      $clip_manager = ClipManager.new(nil, CLIP_MANAGER_IS_STOP)
-    else
-      $clip_manager = ClipManager.new
-    end
-  end
+  $clip_manager = ClipManager.new(
+    Object.const_defined?("CLIP_MANAGER_START_TIME") ? CLIP_MANAGER_START_TIME : nil,
+    Object.const_defined?("CLIP_MANAGER_IS_STOP") ? CLIP_MANAGER_IS_STOP : nil,
+    Object.const_defined?("CLIP_MANAGER_IS_LOOP") ? CLIP_MANAGER_IS_LOOP : nil,
+  )
 end
